d1b2aebf76f771a8ad34bb898c9a2bbc0846957a
swarupyeole11 <swarupyeole11@gmail.com>
2023-02-10 17:38:20 +0530

    Completed the Reas Investemnt Strategy Component

--- src/components/DashBoardButton/DashBoardButton.tsx
+++ src/components/DashBoardButton/DashBoardButton.tsx
@@ -10,9 +10,9 @@ interface DashboardButtonInfo {
 const DashBoardButton: React.FC<DashboardButtonInfo> = ({ imageUrl, text }) => {
   return (
     <>
-      <button className="flex p-1 text-[15] border-white border rounded-3xl">
-        <div className="mr-4"> {text}</div>
-        <img src={imageUrl} alt="Uparrow" />
+      <button className="flex p-1 text-4 border-white border rounded-3xl">
+        <div className="mr-6 ml-1"> {text}</div>
+        <img className='h-[20px] w-[20px]' src={imageUrl} alt="Uparrow" />
       </button>
     </>
   );
